0b76e1cc67f546cf1a4d87fa2b8ca204c14c0c45
Viktor Gievoi <gievoi.v@gmail.com>
2025-08-16 19:07:13 +0200

    Keep only 3 versions

--- hosts/workbench/configuration.nix
+++ hosts/workbench/configuration.nix
@@ -20,6 +20,7 @@
 
   networking.hostName = "workbench";
   boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.configurationLimit = 3;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
