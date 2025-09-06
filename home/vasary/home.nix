aee158a0617cbc4c55a7b140e89e5bb2120cb42b
Viktor Gievoi <gievoi.v@gmail.com>
2025-09-06 12:26:08 +0200

    Update

--- home/vasary/home.nix
+++ home/vasary/home.nix
@@ -59,13 +59,8 @@
       disable-remove-shadow = false;
       input-always-show = false;
       input-show-selected = true;
-      last-unsafe-state = false;
-      list-buttons = "[{\"name\":\"SystemItem\",\"title\":null,\"visible\":true},{\"name\":\"OutputStreamSlider\",\"title\":null,\"visible\":true},{\"name\":\"InputStreamSlider\",\"title\":null,\"visible\":false},{\"name\":\"St_BoxLayout\",\"title\":null,\"visible\":true},{\"name\":\"BrightnessItem\",\"title\":null,\"visible\":false},{\"name\":\"NMWiredToggle\",\"title\":null,\"visible\":false},{\"name\":\"NMWirelessToggle\",\"title\":\"Wi-Fi\",\"visible\":true},{\"name\":\"NMModemToggle\",\"title\":null,\"visible\":false},{\"name\":\"NMBluetoothToggle\",\"title\":null,\"visible\":false},{\"name\":\"NMVpnToggle\",\"title\":null,\"visible\":false},{\"name\":\"BluetoothToggle\",\"title\":\"Bluetooth\",\"visible\":true},{\"name\":\"PowerProfilesToggle\",\"title\":\"Power Mode\",\"visible\":true},{\"name\":\"NightLightToggle\",\"title\":\"Night Light\",\"visible\":true},{\"name\":\"DarkModeToggle\",\"title\":\"Dark Style\",\"visible\":true},{\"name\":\"KeyboardBrightnessToggle\",\"title\":\"Keyboard\",\"visible\":true},{\"name\":\"RfkillToggle\",\"title\":\"Airplane Mode\",\"visible\":true},{\"name\":\"RotationToggle\",\"title\":\"Auto Rotate\",\"visible\":false},{\"name\":\"DndQuickToggle\",\"title\":\"Do Not Disturb\",\"visible\":true},{\"name\":\"BackgroundAppsToggle\",\"title\":\"No Background Apps\",\"visible\":false},{\"name\":\"MediaSection\",\"title\":null,\"visible\":false},{\"name\":\"Notifications\",\"title\":null,\"visible\":false}]";
       notifications-enabled = true;
-      notifications-hide-when-no-notifications = true;
-      output-show-selected = true;
-      volume-mixer-enabled = true;
-      volume-mixer-position = "top";
+      notifications-hide-when-no-notifications = false;
     };
     "org/gnome/shell" = {
       disable-user-extensions = false;
@@ -74,6 +69,7 @@
         "burn-my-windows@schneegans.github.com"
         "clipboard-indicator@tudmotu.com"
         "advanced-weather@sanjai.com"
+        "appindicatorsupport@rgcjonas.gmail.com"
       ];
     };
   };
@@ -97,16 +93,20 @@
       direnv
       unzip
       nixfmt-rfc-style
+
       jetbrains.idea-community-bin
       jetbrains.datagrip
       jetbrains.phpstorm
       jetbrains.jdk
+
       telegram-desktop
+      signal-desktop
 
       gnomeExtensions.burn-my-windows
       gnomeExtensions.clipboard-indicator
       gnomeExtensions.quick-settings-tweaker
       gnomeExtensions.advanced-weather-companion
+      gnomeExtensions.appindicator
     ];
   };
 
